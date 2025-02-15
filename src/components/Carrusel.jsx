f058a06edb63cd6a04f19429494237163c3d15be
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-02-14 23:13:37 -0500

    agrego el archivo Carrusel.jsx

---
+++ src/components/Carrusel.jsx
@@ -0,0 +1,16 @@
+// Importa los componentes de swiper react
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Importa los modulos requeridos del swiper
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+
+// Importa los estilos del swiper
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+const Carrusel = () => {
+
+}
+
+export default Carrusel;
