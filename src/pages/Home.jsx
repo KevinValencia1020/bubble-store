8d569435e3c0e3c4eba1752a080490a3ed7f48b3
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-02-18 12:40:07 -0500

    Creo la estructura del html del banner envios gratis

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,20 @@ const HomePage = () => {
 
   return(
     <>
-      <Carrusel/>
+      <div className="home-main">
+        <Carrusel/>
+
+        <div className="home-banner__container">
+          <img src="/images/banner/Banner Envio Gratis.png" alt="" className="home-banner__img" />
+        </div>
+
+        <p className="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio vero accusamus sunt adipisci, dolor quam aliquam! Nam ipsam corporis et dignissimos error corrupti a illo itaque sed. Nesciunt, deserunt debitis?
+        </p>
+
+      </div>
+
+
     </>
   );
 
