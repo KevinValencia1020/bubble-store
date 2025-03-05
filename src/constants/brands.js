d06d0f9837ddc67d709d4c01ca2eb594c0149cb0
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-04 22:33:52 -0500

    Archivo brands.js el encargado de contener los logos de las marcas

---
+++ src/constants/brands.js
@@ -0,0 +1,10 @@
+const brands = [
+  {image: "https://i.pinimg.com/736x/d8/8f/32/d88f3243954f744b4b212ef93581a124.jpg"},
+  {image: "https://i.pinimg.com/736x/37/5f/15/375f154fa0cd198450e761df4f192470.jpg"},
+  {image: "https://i.pinimg.com/736x/cd/06/95/cd0695814f175babfbcefe0e1b96fa38.jpg"},
+  {image: "https://www.icontec.org/wp-content/uploads/2023/03/Logos-04.png"},
+  {image: "https://cdn.worldvectorlogo.com/logos/lg-electronics.svg"},
+  {image: "https://static.vecteezy.com/system/resources/previews/020/927/282/non_2x/lenovo-logo-brand-phone-symbol-name-black-design-china-mobile-illustration-free-vector.jpg"},
+];
+
+export default brands;
