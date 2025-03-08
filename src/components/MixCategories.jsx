e401ebf092bae16287a3f4440382d47318c87401
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-07 19:04:23 -0500

    En la linea 8/14 reemplazo <div> por <li>

--- src/components/MixCategories.jsx
+++ src/components/MixCategories.jsx
@@ -5,13 +5,13 @@ const MixCategories = () => {
     <div className="home-categ">
       <ul className="mix-categ">
         {mixCategories.map((item, index) => (
-          <div key={index} className="mix-categ__container">
+          <li key={index} className="mix-categ__container">
             <img src={item.img} alt="" className="mix-categ__img" />
             <p className="mix-categ__name">
               {item.name}
             </p>
             <box-icon name = "chevron-right" className="mix-categ__icon"></box-icon>
-          </div>
+          </li>
         ))}
       </ul>
     </div>
