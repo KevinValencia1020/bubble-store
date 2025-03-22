ff7bb0cdb17e6a77929f7a2c739ef133fe0a8ba4
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-21 23:54:47 -0500

    Migracion de tailwind al archivo jsx de header y integracion de las clases del header module

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
-// import "../styles/header.css";
 import SearchSection from "../SearchSection/SearchSection";
+import styleGlobal from "../../../styles/global.module.css";
+import styleHeader from "./header.module.css";
+import clsx from "clsx";
 
 const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
   // estructura para tener el control en la caja de texto para darle utilidad a la X (clear)
@@ -47,35 +49,38 @@ const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
 
   return (
     <>
-      <header className="hero-main">
-        <section className="hero-primary">
-          <div className={`hero-content`}>
+      <header className="hero-main transition-all duration-300 w-full z-40">
+
+        <section className="hero-primary relative flex justify-between items-center bg-color-primario w-full py-4 overflow-hidden">
+
+          <div className={`hero-content relative flex justify-between items-center gap-2 ${styleGlobal.containerContent} transition-transform duration-300 ease-in-out`}>
+
             {/* Boton para cerrar la busqueda */}
-            <button
-              className={`close-search ${activeTab === "Buscar" ? "close-search__open" : "close-search__close"}`}
+            <buttonn
+              className={`close-search absolute top-1 left-0 transition-all duration-300 ease-in-out z-40 ${activeTab === "Buscar" ? "translate-x-0 opacity-100 -left-2" : "translate-x-full opacity-0"}`}
               onClick={(e) => {
                 e.stopPropagation();
                 handleCloseSearch();
               }}
             >
               <box-icon name="chevron-left"></box-icon>
-            </button>
+            </buttonn>
             {/* Logo se oculta cuando la busqueda esta activa */}
 
             <a
               href="#"
-              className={`hero-logo ${
+              className={`hero-logo text-color-secundario no-underline font-medium tracking-widest transition-opacity duration-300 ease-in-out ${
                 activeTab === "Buscar"
-                  ? "hero-logo__active"
-                  : "hero-logo__close"
+                  ? "opacity-0 max-w-0"
+                  : "opacity-100"
               }`}
             >
               BubbleStore
             </a>
-            {/*})}*/}
+
             <form
               method="get"
-              className={`hero-form ${activeTab === "Buscar" ? "active" : ""}`}
+              className={clsx(styleGlobal.containerContent, ["flex", "flex-nowrap", "justify-end", "items-center", "relative", "transition-all", "duration-300", "ease-in-out"],activeTab === "Buscar" && styleHeader.heroFormActive)}
             >
               <input
                 type="text"
@@ -88,13 +93,11 @@ const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
                   openSearch();
                 }}
                 ref={inputRef}
-                className={`hero-input`}
+                className={`${styleHeader.heroInput} relative w-[90%] border-none outline-none p-1 pr-9 pl-2 rounded-2xl transition-all duration-300 ease-in-out`}
               />
 
               <button
-                className={`hero-button hero-button__search ${
-                  activeTab === "Buscar" ? "active" : ""
-                }`}
+                className={`${styleHeader.heroButton} ${styleHeader.heroButtonSearch}`}
                 onClick={(e) => {
                   e.preventDefault();
                   e.stopPropagation();
@@ -110,7 +113,7 @@ const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
                     e.stopPropagation();
                     handleClear();
                   }}
-                  className="hero-button hero-button__clear"
+                  className={`${styleHeader.heroButton} ${styleHeader.heroButtonClear}`}
                 >
                   <box-icon name="x"></box-icon>
                 </button>
@@ -120,14 +123,13 @@ const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
         </section>
       </header>
 
-      {/* {activeTab === "Buscar" && ( */}
+
 
       <SearchSection
         isActive={activeTab === "Buscar"}
         closeSearch={handleCloseSearch}
         isExpanded={activeTab}
       />
-      {/* )} */}
     </>
   );
 };
