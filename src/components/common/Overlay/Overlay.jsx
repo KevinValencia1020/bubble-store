b38738a0939ba85b769150b75725d126591e5980
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-22 21:27:15 -0500

    Migracion de tailwind al archivo jsx Overlay

--- src/components/common/Overlay/Overlay.jsx
+++ src/components/common/Overlay/Overlay.jsx
@@ -3,7 +3,7 @@ import React from "react";
 
 const Overlay = ({isActive, onClick}) => {
   return(
-    <div className={`overlay ${isActive ? "overlay-open" : "overlay-close"}`} 
+    <div className={`overlay fixed top-0 left-0 w-full h-screen bg-black z-30 transition-opacity duration-300 ease-in-out ${isActive ? "opacity-50" : "opacity-0 cursor pointer-events-none"}`} 
     onClick={onClick}>
     </div>
   );
