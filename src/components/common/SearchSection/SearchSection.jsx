2f3e50c54302f80fa326a7eb404d14d638eaffdc
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-23 19:06:12 -0500

    Migracion de tailwind al archivo SearchSection directamente al jsx

--- src/components/common/SearchSection/SearchSection.jsx
+++ src/components/common/SearchSection/SearchSection.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useRef } from "react";
-// import "../styles/searchSection.css";
+import "@/styles/searchSection.css";
 
 const SearchSection = ({ isActive }) => {
   const [showSection, setShowSection] = useState(false);
@@ -14,12 +14,16 @@ const SearchSection = ({ isActive }) => {
       // Retraso breve para activar la transicion
       setTimeout(() => setShowSection(true), 50);
     } else {
-      setShowSection(false); //Animacion de salida
-      setTimeout(() => setIsVisible(false), 300); // Desmonta el componente despues de 300ms
+      setShowSection(false);
+      setTimeout(() => {
+        setIsVisible(false);
+      }, 300); // Desmonta el componente despues de 300ms
     }
     /* Nota: Al cerrar (cuando closeSearch es llamado desde Header), 
     la clase se cambia en Header y la animación se maneja en CSS.*/
   }, [isActive]);
+
+  let popularTags = ["Celulares", "Televisores", "Computadores", "Tablets"]
 
   if (!isVisible) return null;
 
@@ -27,23 +31,24 @@ const SearchSection = ({ isActive }) => {
     <>
       <div
         ref={searchSectionRef}
-        className={`search-section ${
-          isActive ? "search-open" : "search-close"
+        className={`search-section absolute top-16 left-0 w-full h-screen bg-color-secundario transition-all duration-300 ease-in-out z-40 ${
+          isActive ? "translate-y-0 opacity-100" : "translate-y-full pointer-events-none opacity-0"
         }`}
       >
-        <div className="search-container">
-          <div className={`search-results ${showSection ? "search-results__open" : "search-results__close"}`}>
-            <div className="popular-searches">
-              <p className="popular-title">Lo más buscado</p>
+        <div className={styleGlobal.containerContent}>
+          <div className={`search-results mt-4 transition-all duration-300 ease-in-out ${showSection ? "translate-x-0 opacity-100" : "translate-x-full pointer-events-none opacity-0"}`}>
 
-              <div className="popular-tags">
-                <span className="popular-tag">Celulares</span>
+            <div className="popular-searches text-sm font-semibold mb-2">
 
-                <span className="popular-tag">Televisores</span>
+              <p className="popular-title font-semibold text-lg">Lo más buscado</p>
 
-                <span className="popular-tag">Computadores</span>
+              <div className="popular-tags flex flex-wrap gap-2 mt-3">
 
-                <span className="popular-tag">Tablest</span>
+                {popularTags.map((tag, index) => 
+
+                <span key={index} className="popular-tag bg-gray-200 px-3 rounded text-sm">{tag}</span>
+                )}
+
               </div>
             </div>
           </div>
