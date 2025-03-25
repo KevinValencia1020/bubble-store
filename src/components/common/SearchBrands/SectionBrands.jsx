1212e504c47998fc6d52e9c48db16666d06f7a66
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-25 10:37:06 -0500

    Migracion de tailwind al archivo SectionBrands directamente al jsx

--- src/components/common/SearchBrands/SectionBrands.jsx
+++ src/components/common/SearchBrands/SectionBrands.jsx
@@ -1,20 +1,28 @@
 import brands from "@/constants/brands";
-// import "../styles/sectionBrands.css";
+import styleBrands from "./searchBrands.module.css";
+
 const SectionBrands = () => {
   return(
     <>
-      <section className="section-brands">
-        <div className="section-brands__container">
-          <h3 className="section-brands__title">Lleva a casa las mejores marcas!</h3>
+      <section className="section-brands mt-4">
+        <div className="section-brands__container w-[90%] my-3 mx-auto">
 
-            <ul className="section-brands__ul">
+          <h3 className="section-brands__title text-lg font-semibold mb-4">Lleva a casa las mejores marcas!</h3>
+
+            <ul className={styleBrands.sectionBrandsUl}>
+
               {brands.map((item, i) => (
-                <li key={i}     className="section-brands__li">
-                  <a href="#" className="section-brands__link">
-                    <img src={item.image} alt="" className="section-brands__img" />
+
+                <li key={i} className="section-brands__li">
+
+                  <a href="#" className="section-brands__link block w-20 h-20 overflow-hidden border rounded-lg">
+
+                    <img src={item.image} alt="" className="section-brands__img block w-full h-full object-fill" />
                   </a>
                 </li>
+
               ))}
+
             </ul>
         </div>
       </section>
