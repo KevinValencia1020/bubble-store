ff1563d42ee9cdaa62e8ef476ed0e8d8fe992197
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-03-28 13:01:01 -0500

    Correccion en la linea 59 en la etiqueta button

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -56,7 +56,7 @@ const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
           <div className={`hero-content relative flex justify-between items-center gap-2 ${styleGlobal.containerContent} transition-transform duration-300 ease-in-out`}>
 
             {/* Boton para cerrar la busqueda */}
-            <buttonn
+            <button
               className={`close-search absolute top-1 left-0 transition-all duration-300 ease-in-out z-40 ${activeTab === "Buscar" ? "translate-x-0 opacity-100 -left-2" : "translate-x-full opacity-0"}`}
               onClick={(e) => {
                 e.stopPropagation();
@@ -64,7 +64,7 @@ const HeaderMain = ({ activeTab, closeSearch, openSearch }) => {
               }}
             >
               <box-icon name="chevron-left"></box-icon>
-            </buttonn>
+            </button>
             {/* Logo se oculta cuando la busqueda esta activa */}
 
             <a
