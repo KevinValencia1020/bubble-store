9d3c7d66ee89845c235fbefc78ea3fd5c9f0f137
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-04-03 20:38:47 -0500

    Referencio el footer, ajusto el alto a auto del footer agrego un estado para el acordeon, agrego el wave y creo el footer-social

--- src/components/common/Footer/Footer.jsx
+++ src/components/common/Footer/Footer.jsx
@@ -2,10 +2,15 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css";
 
+import { useRef, useState } from "react";
 import styleGlobal from "../../../styles/global.module.css";
 import styleFooter from "./footer.module.css";
 import AccordionCustomized from "../Accordeon/Accordeon";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
 
 const darkTheme = createTheme({
   palette: {
@@ -15,9 +20,32 @@ const darkTheme = createTheme({
 });
 const Footer = () => {
 
+  // Ref para el contenedor de footer
+  const footerRef = useRef(null);
+
+  // Estado para controlar si el acordeon esta expandido
+  const [expanded, setExpanded] = useState(false);
+
+  const handleAccordionChange = (isExpanded) => {
+
+    setExpanded(isExpanded);
+    
+  };
+
   return (
     <>
-      <div className="footer bg-slate-800 text-white py-4 w-full h-auto my-5">
+
+      <div 
+      ref={footerRef}
+      className="footer relative bg-slate-800 text-white py-4 pb-40 w-full h-auto my-5 mt-28"
+      >
+
+        <div className="wave-footer absolute -top-20 left-0 w-full h-20 bg-cover bg-center" style={
+          { backgroundImage: "url('/images/waves/wave-footer.svg')" }
+          } >
+
+        </div>
+
         <Swiper
           modules={[Autoplay]}
           slidesPerView={"auto"}
@@ -81,18 +109,43 @@ const Footer = () => {
             </div>
           </SwiperSlide>
         </Swiper>
-        
-        <div className="w-full border border-white mt-1"></div>
 
-        <div className="footer-accordion h-screen my-5">
+        <div className="footer-accordion h-auto my-5 transition-all duration-300 ease-in-out">
 
           <ThemeProvider theme={darkTheme}>
 
-            <AccordionCustomized />
+            <AccordionCustomized onExpandedChange={handleAccordionChange}/>
 
           </ThemeProvider>
 
         </div>
+
+        <div className="w-full border border-white mt-1"></div>
+
+        <div className="footer-social">
+
+          <div className={`footer-social__container ${styleGlobal.containerContent}`}>
+
+            <p className="footer-social__tittle mt-4">Siguenos: </p>
+
+            <ul className="footer-social__icons flex flex-row items-center justify-evenly mt-5">
+
+              <li className="footer-social__icon">
+                <FacebookIcon fontSize="large" color="white" />
+              </li>
+
+              <li className="footer-social__icon">
+                <InstagramIcon fontSize="large" color="white"/>
+              </li>
+
+              <li className="footer-social__icon">
+                <XIcon fontSize="large" color="white"/>
+              </li>
+
+            </ul>
+          </div>
+        </div>
+        
       </div>
     </>
   );
