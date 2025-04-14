e94f2ecc102f475ef278260c4b50efa3d072b3ae
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-04-14 13:25:00 -0500

    Creo una lista de objetos y simulo algunos productos para la busqueda

---
+++ src/constants/productsSearch.js
@@ -0,0 +1,24 @@
+const productsSearch = [
+  {
+    id: 1,
+    name: "iPhone 13 Pro Max",
+  },
+  {
+    id: 2,
+    name: "Samsung Galaxy S22 Ultra",
+  },
+  {
+    id: 3,
+    name: "Xiaomi Mi 12 Pro",
+  },
+  {
+    id: 4,
+    name: "OnePlus 9 Pro",
+  },
+  {
+    id: 5,
+    name: "Google Pixel 6 Pro",
+  },
+];
+
+export default productsSearch;
