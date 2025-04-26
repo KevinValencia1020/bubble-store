1379bb9846d5c5e022cbcf52682a4a45c174e943
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-04-26 11:35:15 -0500

    Elimino el import del Layout y del return

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,10 @@
 "use client";
 import Home from "@/components/page/home/Home";
-import Layout from "@/components/layout/Layout/Layout";
 
 export default function Page() {
   return(
     <>
       
-      <Layout/>
       <Home/>
 
     
