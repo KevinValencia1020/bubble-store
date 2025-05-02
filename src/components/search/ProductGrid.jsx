04f9edc9c9e3021a34eaaf4e01be2ad69730a141
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-05-01 21:36:26 -0500

    Mejoramiento de estructuracion para mostrar los cards de los productos

--- src/components/search/ProductGrid.jsx
+++ src/components/search/ProductGrid.jsx
@@ -10,20 +10,23 @@ export default function ProductGrid({ products }) {
       <div className="product-grid">
 
         {/* Itera sobre los productos y renderiza una tarjeta para cada uno */}
-        {products.map((product) => (
-          
-          <ProductCard
+        
+        <ul>
+          {products.map((product) => (
 
-            key={product.id} // clave unica para cada producto
-            id={product.id}
-            name={product.name}
-            price={product.price}
-            image={product.image}
-            brand={product.brand}
-            onAddToCart={product.onAddToCart} // funcion para añadir al carrito
+            <li key={product.id}>
 
-          />
-        ))}
+              <ProductCard 
+                id={product.id}
+                name={product.name}
+                price={product.price}
+                image={product.image}
+                brand={product.brand}
+                onAddToCart={product.onAddToCart} // funcion para añadir al carrito
+              />
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
