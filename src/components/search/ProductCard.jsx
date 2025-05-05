f413a04dc9ad16360d1ec863ee81ac44bbec392a
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-05-04 19:52:17 -0500

    Estableciendo los estilos para las cards de los productos

--- src/components/search/ProductCard.jsx
+++ src/components/search/ProductCard.jsx
@@ -1,35 +1,44 @@
 import { IconButton } from "@mui/material";
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
+import AddShoppingCartIcon from "@mui/icons-material/AddShoppingCart";
 
 // Componente que renderiza una tarjeta individual de producto
-export default function ProductCard ({ id, name, price, image, brand, onAddToCart }) {
-
+export default function ProductCard({
+  id,
+  name,
+  price,
+  image,
+  brand,
+  onAddToCart,
+}) {
   return (
-    <div className="product-card">
+    <>
+      <div className="border relative rounded-xl my-4">
 
-      <img src={image} alt={name} className="product-card__image" />
+        <span className="product-card__brand absolute -top-4 left-2 z-10 bg-white border border-color-primario rounded-md text-color-primario font-bold text-gray-500 px-2 py-[2px]"
+        >
+          {brand}
+        </span>
+        <div className="product-card w-[90%] mx-auto my-0 relative flex flex-col gap-2">
+          <img src={image} alt={name} className="product-card__image" />
 
-      <span className="product-card__brand">
-        {brand}
-      </span>
 
-      <h3 className="product-card__name">
-        {name}
-      </h3>
+          <h3 className="product-card__name">{name}</h3>
 
-      <p className="product-card__price">
-        ${price}
-      </p>
+          <p className="product-card__price">${price}</p>
 
-      {/* Boton para añadir al carrito */}
-      <IconButton
-        className="product-card__button bg-color-primario"
-        onClick={() => onAddToCart(id)}
-      >
-        <AddShoppingCartIcon fontSize="large" sx={{ color: "var(--color-primary)"}}/>
-        
-      </IconButton>
-
-    </div>
+          {/* Boton para añadir al carrito */}
+          <IconButton
+            className="product-card__button !bg-color-primario flex flex-col items-center justify-center gap-1 absolute bottom-1 left-[80%] px-2"
+            sx={{borderRadius: "10px", width: 50, height: 48}}
+            onClick={() => onAddToCart(id)}
+          >
+            <AddShoppingCartIcon
+              fontSize="medium"
+              sx={{ color: "var(--color-secundary)" }}
+            />
+          </IconButton>
+        </div>
+      </div>
+    </>
   );
 }
