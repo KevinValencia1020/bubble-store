cfc713725b00adef1197cbf439dd8d4c4cd8bf67
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-05-19 22:00:44 -0500

    Mover archivos a la carpeta frontend

--- src/constants/bannerGrid.js
+++
@@ -1,9 +0,0 @@
-
-export const bannerGrid = [
-  {image: "/images/banner/Asus-vivobook.webp"},
-  {image: "/images/banner/Hp-14.webp"},
-  {image: "/images/banner/smart-watch.jpg"},
-  {image: "/images/banner/Lenovo-ideapad-slim3.webp"},
-  {image: "/images/banner/Macbook-Air.webp"},
-  {image: "/images/banner/Lenovo-gamer.webp"}
-];
