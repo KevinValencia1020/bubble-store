39f0dddac1b61a14972f4999992d695770a1c92b
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-06-08 22:22:45 -0500

    Aplicando y estrableciendo los middlewares globales

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,10 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+
+const app = express();
+
+// Middleware
+app.use(cors()); // Permite solicitudes desde otros orígenes (frontend)
+app.use(express.json()); // Analiza el cuerpo de las solicitudes JSON
+app.use(morgan('dev')); // Registra las solicitudes HTTP en la consola
