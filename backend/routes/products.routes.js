4a774f1659ddbbd74cf7914608dfe5afdb4f0de0
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-07-10 22:29:54 -0500

    Defino el router con el get para obtener los productos por categoria

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -3,3 +3,6 @@ import { getProductsByCategory } from "../controllers/products.controller";
 
 const router = Router();
 
+router.get('/category/:category', getProductsByCategory); // Ruta para obtener productos por categoría (por nombre de categoría)
+
+export default router; // Exporta el router para que pueda ser utilizado en otros archivos
