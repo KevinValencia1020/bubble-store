f0d512166a718a01c07f9d3b60d39d16f9b6904c
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-07-13 18:53:56 -0500

    Importo el dotenv al archivo server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-require('dotenv').config(); // Carga las variables de entorno desde el archivo .env
-import app from './app';
+import 'dotenv/config' // Carga las variables de entorno desde el archivo .env
+import app from './app.js';
 
 // Escucha el puerto desde la variable de entorno o usa el puerto 4000 por defecto
 const PORT = process.env.PORT || 4000;
