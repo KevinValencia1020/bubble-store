9dcd909ba51971e2a7448a77ec9a5b8d8445dd8d
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-07-13 22:56:43 -0500

    Ajusto los espacios para ordenar el codigo

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -6,24 +6,19 @@ export const searchProducts = async (req, res, next) => {
     const { category, keyword, minPrice, maxPrice, brand } = req.query;
 
     let query = `
-        SELECT p.product_name, 
-        p.price, 
-        p.brand, 
-        p.stock_quantity,
-        pi.image_url AS thumbnail
-        FROM products p
-        INNER JOIN product_images pi ON pi.product_id = p.product_id AND pi.is_thumbnail = true
-        INNER JOIN categories c ON c.category_id = p.category_id
-        WHERE 1 = 1
-      `;
+    SELECT p.product_name, 
+    p.price, 
+    p.brand, 
+    p.stock_quantity,
+    pi.image_url AS thumbnail
+    FROM products p
+    INNER JOIN product_images pi ON pi.product_id = p.product_idAND pi.is_thumbnail = true
+    INNER JOIN categories c ON c.category_id = p.category_id
+    WHERE 1 = 1
+    `;
 
-    query = `
-        SELECT p.product_id, p.product_name, p.price, p.brand, p.stock_quantity
-        FROM products p
-        INNER JOIN product_images pi ON pi.product_id = p.product_id AND pi.is_thumbnail = true
-        ORDER BY p.product_name
-      `;
-    values = [];
+    const values = [];
+
 
     const { rows } = await pool.query(query, values);
     res.json(rows);
