e96039b89680baa171ff21c4ff39d91c5e5893df
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-07-13 22:25:43 -0500

    Elimino el if-else

--- backend/controllers/products.controller.js
+++ backend/controllers/products.controller.js
@@ -1,14 +1,13 @@
-import pool from "../config/db.js";
+import pool from '../config/db.js';
 
 export const searchProducts = async (req, res, next) => {
-  
+
   try {
     const { category, keyword, minPrice, maxPrice, brand } = req.query;
 
     let query, values; // Define la consulta SQL y los valores a utilizar
 
-    if (category && category !== "all") {
-      query = `
+    query = `
         SELECT p.product_name, 
         p.price, 
         p.brand, 
@@ -17,19 +16,16 @@ export const searchProducts = async (req, res, next) => {
         FROM products p
         INNER JOIN product_images pi ON pi.product_id = p.product_id AND pi.is_thumbnail = true
         INNER JOIN categories c ON c.category_id = p.category_id
-        WHERE LOWER(c.name_category) = $1
-        ORDER BY p.product_name;
+        WHERE 1 = 1
       `;
-      values = [category.toLowerCase()];
-    } else {
-      query = `
+
+    query = `
         SELECT p.product_id, p.product_name, p.price, p.brand, p.stock_quantity
         FROM products p
         INNER JOIN product_images pi ON pi.product_id = p.product_id AND pi.is_thumbnail = true
         ORDER BY p.product_name
       `;
-      values = [];
-    }
+    values = [];
 
     const { rows } = await pool.query(query, values);
     res.json(rows);
@@ -37,4 +33,4 @@ export const searchProducts = async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-}
+};
