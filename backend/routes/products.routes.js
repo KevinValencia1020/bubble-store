a9171f83a7974b1bfda9ee6522b1500b0a4d88f4
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-07-15 12:54:24 -0500

    Agrego la extension .js en el import de searchProducts

--- backend/routes/products.routes.js
+++ backend/routes/products.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { searchProducts } from "../controllers/products.controller";
+import { searchProducts } from "../controllers/products.controller.js";
 
 const router = Router();
 
