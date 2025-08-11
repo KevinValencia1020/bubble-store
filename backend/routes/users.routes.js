7fcffaed02b274210702bd73fceb1c99e7dcf95a
Kevin valencia <kevintovarvalencia21@gmail.com>
2025-08-11 09:47:38 -0500

    Defino las rutas para obtener perfil, actualizarlo o cambio de contraseña

--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,11 +1,19 @@
 import express from 'express';
 const router = express.Router();
 import * as usersController from '../controllers/users.controller.js';
+import auth from '../middleware/auth.js';
 
 // Ruta para login (correo y contraseña)
 router.post('/login', usersController.loginUser);
 
 // Ruta para registro de usuario
 router.post('/register', usersController.registerUser);
+
+// Obtener perfil
+router.get('/profile', auth, usersController.getProfile);
+// Actualizar perfil
+router.put('/profile', auth, usersController.updateProfile);
+// Cambiar contraseña
+router.post('/change-password', auth, usersController.changePassword);
 
 export default router;
